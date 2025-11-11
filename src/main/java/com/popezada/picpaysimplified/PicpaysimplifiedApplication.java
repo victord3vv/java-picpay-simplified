05a0335e0e9b8337b6f7b3551a28ca0f0227d5a2
Vitinho <victorhugofake12@gmail.com>
2025-11-11 19:15:54 -0300

    Initial commit: Project generated with Spring Initializr

---
+++ src/main/java/com/popezada/picpaysimplified/PicpaysimplifiedApplication.java
@@ -0,0 +1,13 @@
+package com.popezada.picpaysimplified;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpaysimplifiedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpaysimplifiedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/popezada/picpaysimplified/PicpaysimplifiedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.popezada.picpaysimplified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaysimplifiedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
